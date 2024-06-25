7f6a8fc0d21ef8fbd22e933415d774863943da90
obabaka <123298875+obabaka@users.noreply.github.com>
2024-06-25 19:39:35 +1000

    Update program.js

--- program.js
+++ program.js
@@ -1,26 +1,32 @@
 let result = (str) => {
+    if (str == '') {
+        return '<h1>Строка пуста'
+    }
     document.write(`<h1>Исходная строка: ${str}<br>`);
     let str1, str2, res = '';
     str = str.trim();
-    while (str.length > 0) {
+    while ((str.length > 0) && (str.indexOf(' ') != -1)) {
+        str1 = str.slice(0, str.indexOf(' '));
+        str = str.slice(str.indexOf(' ') + 1);
         if (str.indexOf(' ') == -1) {
-            break;
-        }else {
-            str1 = str.slice(0, str.indexOf(' '));
-            str = str.slice(str.indexOf(' ') + 1);
+            str2 = str;
+            str = '';
+        }else{
             str2 = str.slice(0, str.indexOf(' '));
             str = str.slice(str.indexOf(' ') + 1);
-            if (!isNaN(str2) && isNaN(str1)) {
-                res += str1 + ' ';
-            }else {
-                str = str2 + ' ' + str;
-            }
+        }
+        if ((!isNaN(str2)) && (isNaN(str1))) {
+            res += str1 + ' ';
+        }else {
+            str = str2 + ' ' + str;
+            str = str.trim();
         }
     }
-    document.write(`<h1>Слова после которых встречаются числа: `);
-    return res;
+    if (res == '') {
+         return 'Слов удовлетворяющих условию - нет'
+    } else {
+        return `<h1>Слова после которых встречаются числа: ${res}`
+    }
 };
-let s = 'aaa 444 bbb ccc 33 ddd';
+let s = 'aaa 444 bbb 6666 ccc ccc 444 444 444';
 document.write(result(s));
-
-
